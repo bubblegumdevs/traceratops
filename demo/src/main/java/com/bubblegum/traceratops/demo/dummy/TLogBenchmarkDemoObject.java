e6e22821d6f673fd2176be14086966d6af536932
Girish Kamath <girishk91@gmail.com>
2016-01-19 05:11:08 +0530

    Added benchmarking tool in demo

---
+++ demo/src/main/java/com/bubblegum/traceratops/demo/dummy/TLogBenchmarkDemoObject.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 Bubblegum Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bubblegum.traceratops.demo.dummy;
+
+import com.bubblegum.traceratops.sdk.client.annotations.TLogEntry;
+
+public class TLogBenchmarkDemoObject {
+
+    @TLogEntry("name")
+    private String name = "Girish";
+    @TLogEntry("Long string")
+    private String longString = "GirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGir" +
+            "ishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishG" +
+            "irishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiri" +
+            "shGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGir" +
+            "ishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGir" +
+            "ishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirish" +
+            "GirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishG" +
+            "irishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirish" +
+            "GirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiri" +
+            "shGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiri" +
+            "shGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirish" +
+            "GirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiri" +
+            "shGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiri" +
+            "shGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirish" +
+            "GirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGi" +
+            "rishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishG" +
+            "irishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGiris" +
+            "hGirishGirishGirishGiris" +
+            "hGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirishGirish";
+    private String password = "Hidden";
+    @TLogEntry("ID number")
+    private int id = 73;
+
+}
